cae3132a3e999595c53dc7579f2343bc31530338
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-09-28 20:10:56 +0200

    I made function to remove smallest number in array.

--- Open-Lab-05.07/Numbers.cs
+++ Open-Lab-05.07/Numbers.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace Open_Lab_05._07
 {
@@ -6,7 +8,20 @@ namespace Open_Lab_05._07
     {
         public int[] RemoveSmallest(int[] nums)
         {
-            throw new NotImplementedException();
+            List<int> ret = new List<int>();
+            int smallest = nums[0];
+            bool once = false;
+            foreach (int num in nums) if (num < smallest) smallest = num;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == smallest && !once)
+                    once = true;
+
+                else
+                    ret.Add(nums[i]);
+
+            }
+            return ret.ToArray();
         }
     }
 }
